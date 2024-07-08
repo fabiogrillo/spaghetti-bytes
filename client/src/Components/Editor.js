5ed76d14a89b0e0a9409d65d91e071113913536a
fabiogrillo <fabiogrillo123@gmail.com>
2024-07-08 12:52:51 +0200

    completed Editor.js

--- client/src/Components/Editor.js
+++ client/src/Components/Editor.js
@@ -1,39 +1,54 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { motion } from "framer-motion";
+
+const Font = Quill.import("formats/font");
+Font.whitelist = ["sans-serif", "serif", "monospace"];
+Quill.register(Font, true);
 const StoryEditor = () => {
   const [value, setValue] = useState("");
 
   const modules = {
-    toolbar: [
-      [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
+    toolbar: {
+      container: [
+        [
+          {
+            font: Font.whitelist,
+          },
+        ],
+        [{ size: ["small", false, "large", "huge"] }],
+        ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        [{ color: [] }, { background: [] }], // Aggiunge i colori per il testo e lo sfondo
+        [{ align: [] }], // Aggiunge i pulsanti di allineamento del testo
+        ["link", "image"],
+        ["clean"],
       ],
-      ["link", "image"],
-      ["clean"],
-    ],
-    history: {
-      delay: 2000,
-      userOnly: true,
-    }
+    },
   };
 
-
   return (
-    <div className="card w-full min-h-96 bg-carolina-blue p-6">
+    <motion.div
+      className="card w-full min-h-96 bg-carolina-blue p-6"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ ease: "anticipate", duration: 1.5, x: { duration: 0.5 } }}
+    >
       <ReactQuill
+        className="custom-quill bg-neutral-content text-slate-800 shadow-lg rounded-2xl"
         theme="snow"
         value={value}
         onChange={setValue}
         modules={modules}
         placeholder="Type your story here..."
       />
-    </div>
+    </motion.div>
   );
 };
 
