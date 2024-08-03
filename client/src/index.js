376966d012c18abd471f4d843a4b6cbc589aa1f7
fabiogrillo <fabiogrillo123@gmail.com>
2024-08-03 20:18:13 +0200

    speed insights inserted

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
